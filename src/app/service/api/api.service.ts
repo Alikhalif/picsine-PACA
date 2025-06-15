e83aa19ddc0e3f3c88022f11f7f5606c68c9960e
Alikhalif <abdelalikhalif1999@gmail.com>
2025-06-15 19:25:01 +0100

    salit

--- src/app/service/api/api.service.ts
+++ src/app/service/api/api.service.ts
@@ -6,9 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  // private baseUrl = 'https://depannage-piscine.com/api';
+  private baseUrl = 'https://depannage-piscine.com/api';
 
-  private baseUrl = 'http://127.0.0.1:3000/api'
+  // private baseUrl = 'http://127.0.0.1:3000/api'
 
 
   constructor(private http: HttpClient) {}
